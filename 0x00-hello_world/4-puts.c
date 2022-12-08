cfd7218e7e0fe776c54a290d2154416b27bea82a
Robelhail <richnrob.2020@gmail.com>
2022-12-08 11:11:41 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - main void
+ * main - Entry point
  *
- * Return: 0 
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
