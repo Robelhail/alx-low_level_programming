1e3f2e4e7a971708de044cf00087d371d36a9670
Robelhail <richnrob.2020@gmail.com>
2022-12-09 16:53:11 -0800

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,29 +8,23 @@
 
 int main(void)
 {
-int tho;
-int hun;
-int ten;
-int bas;
-for (tho = 0 ; tho < 10 ; tho++)
+int i;
+int j;
+for (i = 0; i < 100; i++)
 {
-for (hun = 0 ; hun <= 10 ; hun++)
+for (j = 0; j < 100; j++)
 {
-for (ten = 0 ; ten < 10 ; ten++)
+if (j > i)
 {
-for (bas = 0 ; bas < 10 ; bas++)
-{
-putchar('0' + tho);
-putchar('0' + hun);
-putchar(32);
-putchar('0' + ten);
-putchar('0' + bas);
-if (!(tho == 9 && hun == 8))
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i * 100 + j != 9899)
 {
 putchar(',');
-putchar(32);
-}
-bas++;
+putchar(' ');
 }
 }
 }
