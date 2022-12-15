5ef2e8fe99eb2f6c6806360ec13318e0c97b308e
Robelhail <richnrob.2020@gmail.com>
2022-12-15 03:03:13 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - To Test the function whether it is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
+
