d27a62156c019b0e1d15befceb33b4d3ab06d67c
Robelhail <richnrob.2020@gmail.com>
2022-12-19 06:10:39 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_rev - prints a string, in reverse
+* @s: string
+* return: 0
+*/
+void print_rev(char *s)
+{
+int lnz = 0;
+int o;
+
+while (*s != '\0')
+{
+lnz++;
+s++;
+}
+s--;
+for (o = lnz; o > 0; o--)
+{
+_putchar(*s);
