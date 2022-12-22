c6daf3bdffc519cbfc8fe2a9556d1ec3726fb214
Robelhail <richnrob.2020@gmail.com>
2022-12-22 11:45:35 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-/**
-* print_number - prints an integer
-* @n: the integer to print
-* Return: nothing
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-
-void print_number(int n);
-
-{
-unsigned int k = n;
-
-if (n < 0)
-{
-n *= -1;
-k = n;
-_putchar('-');
-}
-k /= 10;
-if (k != 0)
-print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
