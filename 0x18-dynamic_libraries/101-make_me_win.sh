4f770b736aef8921a806bf0d5f76d66fb09a83bd
Robelhail <richnrob.2020@gmail.com>
2023-03-20 14:20:16 -0700

    dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Robelhail/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://github.com/Robelhail/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
