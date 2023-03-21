32c982695893f943d2849f70fb870c969a3a12eb
Robelhail <richnrob.2020@gmail.com>
2023-03-20 17:16:26 -0700

    fine

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/Robelhail/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottery.so"
-export LD_PRELOAD="/tmp/lottery.so"
+wget -O /tmp/lottery.so https://github.com/Robelhail/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottery.so
+export LD_PRELOAD=/tmp/lottery.so
+
