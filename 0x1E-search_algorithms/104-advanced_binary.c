2729d0c5ff2973757e5ff6d03d8e6f1d1115578e
Robelhail <richnrob.2020@gmail.com>
2023-07-11 04:29:54 -0700

    advanced task

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,64 +1,84 @@
 #include "search_algos.h"
 
 /**
- * rec_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * printer - function that prints current sub array that's being examined
+ * @array: Original int array
+ * @l: int for starting index value of sub-array
+ * @r: int for ending index of sub-array
+ * Return: Void
  */
-int rec_search(int *array, size_t size, int value)
-{
-	size_t half = size / 2;
-	size_t i;
 
-	if (array == NULL || size == 0)
+void printer(int *array, int l, int r)
+{
+	int i;
+
+	printf("Searching in array: ");
+
+	for (i = l; i <= r; i++)
+	{
+		if (i == r)
+		{
+			printf("%d\n", array[i]);
+			return;
+		}
+
+		printf("%d, ", array[i]);
+	}
+}
+
+
+/**
+ * advanced_binary_help - main helper function that searches for a value in a
+ * sorted array of integers using the Binary search algorithm
+ * @array: int  pointer to the first element of the array to search in
+ * @low: int that tracks the lower bound of the sub-array to be checked
+ * @high: int that tracks the upper bound of the sub-array to be checked
+ * @value: is the value to search for
+ * Return: first index where value is located, -1 if array is NULL or
+ * value is not found
+ */
+
+
+int advanced_binary_help(int *array, int low, int high, int value)
+{
+	int mid = 0;
+
+	mid =  low + (high - low) / 2;
+
+	if  (low > high)
 		return (-1);
 
-	printf("Searching in array");
+	printer(array, low, high);
 
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	if (value == array[mid] && value != array[mid - 1])
+		return (mid);
 
-	printf("\n");
+	else if (value < array[mid] || value == array[mid - 1])
+		return (advanced_binary_help(array, low, mid, value));
 
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-	{
-		if (half > 0)
-			return (rec_search(array, half + 1, value));
-		return ((int)half);
-	}
-
-	if (value < array[half])
-		return (rec_search(array, half + 1, value));
-
-	half++;
-	return (rec_search(array + half, size - half, value) + half);
+	else
+		return (advanced_binary_help(array, mid + 1, high, value));
 }
 
 /**
- * advanced_binary - calls to rec_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * advanced_binary - function that searches for a value in a
+ * sorted array of integers using the binary search algorithm that
+ * handles duplicates
+ * @array: int  pointer to the first element of the array to search in
+ * @size: size_t variable of  the number of elements in array
+ * @value: the value to search for
+ * Return: first index where value is located, -1 if array is NULL or
+ * value is not found
  */
+
 int advanced_binary(int *array, size_t size, int value)
 {
-	int index;
+	int high;
 
-	index = rec_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
+	if (array == NULL)
 		return (-1);
 
-	return (index);
+	high = (int) size;
+
+	return (advanced_binary_help(array, 0, high - 1, value));
 }
